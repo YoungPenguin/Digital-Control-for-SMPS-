a577611f4c0dfa39e78448a9bec9609101cc9343
YoungPenguin <56176145+YoungPenguin@users.noreply.github.com>
2020-01-23 15:36:33 +0100

    Update ADC_EPWM_PI.c

--- ADC_EPWM_PI.c
+++ ADC_EPWM_PI.c
@@ -12,7 +12,7 @@
 // Functionality and usability:
 //------------------------------------------------------------------------------
 //
-//  The feedback is on the ADCINA0 AA0 (j3-26
+//  The feedback is on the ADCINA4 AA4 (j1-6)
 //  The PWM out is located on GPIO6 P6 (j2-13)
 //
 //  The PI control algorithm is implemented as a discrete PI with the formula
@@ -79,7 +79,7 @@ double ki   = 1.7131; // ki=kp/ti
 
 int out_max = 297; // 150 kHz - calculated by the TBPRD
 int out_min = 33;
-
+double PI_out      = 0;
 double error       = 0;
 double PI_output   = 0;
 double Ts          = 0.00000666666; // 1/150000  - 150 kHz
@@ -94,7 +94,7 @@ interrupt void adc_isr(void) {
     // PI control algo begin
     error     = Ref_v-adcresult;
     PI_output = (kp*(error-prev_error)+ki*Ts*error)+prev_out;
-    PI_output = TBPRD-PI_output;
+    PI_out = 330-PI_output;
     // anti-windup
     if(PI_output > out_max){
         PI_output = out_max;
@@ -106,7 +106,7 @@ interrupt void adc_isr(void) {
     // defining the u(k-1) and e(k-1)
     prev_out   = PI_output;
     prev_error = error;
-    set_duty(PI_output); //
+    set_duty(PI_out); //
   //  set_duty(PI_output);     // set PWM to the PI regulated value
 
     ADC_clearIntFlag(myAdc, ADC_IntNumber_1);   // Clear ADCINT1 flag
