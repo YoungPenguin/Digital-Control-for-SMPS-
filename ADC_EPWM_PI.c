67ca8c070c541363ca5560f9709001281b1367b0
Justin Jacques <56176145+YoungPenguin@users.noreply.github.com>
2020-08-11 17:56:37 +0200

    Update ADC_EPWM_PI.c

--- ADC_EPWM_PI.c
+++ ADC_EPWM_PI.c
@@ -28,194 +28,214 @@
  * Included header
  *******************************************************************************/
 
-#include "DSP28x_Project.h"               // Device Headerfile and Examples Include File
-#include "f2802x_common/include/adc.h"    // Analog to digital converter
-#include "f2802x_common/include/clk.h"    // clk
-#include "f2802x_common/include/flash.h"  // flash
-#include "f2802x_common/include/gpio.h"   // GPIO pins (used for GPIO6)
+// Code for H bridge buck boost converter PWM
+
+#include "DSP28x_Project.h"     // Device Headerfile and Examples Include File
+
+#include "f2802x_common/include/adc.h"
+#include "f2802x_common/include/clk.h"
+#include "f2802x_common/include/flash.h"
+#include "f2802x_common/include/gpio.h"
 #include "f2802x_common/include/pie.h"
 #include "f2802x_common/include/pll.h"
-#include "f2802x_common/include/timer.h"
-#include "f2802x_common/include/wdog.h"
-#include "f2802x_common/include/pwr.h"
 #include "f2802x_common/include/pwm.h"
+#include "f2802x_common/include/wdog.h"
 
-void pwm_Init_();
+// Prototype statements for functions found within this file.
+#define PWM1_TIMER_TBPRD   0x012C       //TBPRD = 300 == 100Khz
+__interrupt void adc_isr(void);
+void InitEPwm1(void);
+// Global variables used in this example:
+uint16_t LoopCount;
+uint16_t ConversionCount;
+//uint16_t V1, V2, V3;
+uint16_t V4, V5, V6;
+uint16_t Voltage1[20];
+ float Vout_count,Va,sum = 0;
+ float err_5, A_5=0,B_5=0, v_5=0, Vpi_5 = 0;
+static float in_A = 0.0, k_5 = 0.0;
+float V_duty;
+float comp_value, Duty;
+/*** PI controller **/
 
-unsigned int TBPRD = 330; // TBPRD register - the speed of the system - here 150 kHz
-unsigned int CMPA  = 0;
 
-// setup all the handles
-ADC_Handle   myAdc;
-CLK_Handle   myClk;
+    float Kp = 0.1;     // [0] proportional gain
+    float Ki = 0.05;        // [2] integral gain
+    float i10;              // [4] I storage
+    float Umax = 0.99;      // [6] upper saturation limit
+    float Umin = -0.99;     // [8] lower saturation limit
+    float i6;               // [A] saturation storage
+
+ADC_Handle myAdc;
+CLK_Handle myClk;
 FLASH_Handle myFlash;
-GPIO_Handle  myGpio;
-PIE_Handle   myPie;
-TIMER_Handle myTimer;
-CPU_Handle   myCpu;
-PLL_Handle   myPll;
-WDOG_Handle  myWDog;
-PWM_Handle   myPwm4;
-PWR_Handle   myPwr;
+GPIO_Handle myGpio;
+PIE_Handle myPie;
+PWM_Handle myPwm1, myPwm2;
 
-uint16_t Digital_Result = 0;
-
-void disable();
-void enable();
-void ADC_INIT_Fn();
-void ADC_SETUP_Fn();
-void set_duty(int a);
-
-int adcresult = 0;
-int Ref_v     = 110;
-
-// control parameters
-double kp   = 0.00007829;
-double ki   = 1.7131; // ki=kp/ti
-
-int out_max = 297; // 150 kHz - calculated by the TBPRD
-int out_min = 33;
-double PI_out      = 0;
-double error       = 0;
-double PI_output   = 0;
-double Ts          = 0.00000666666; // 1/150000  - 150 kHz
-double prev_out    = 0;
-double prev_error  = 0;
-
-interrupt void adc_isr(void) {
-    Digital_Result = ADC_readResult(myAdc, ADC_ResultNumber_0);
-
-    adcresult = (Digital_Result/12.40909091); // ADC 4095 mapped to TBPRD 330
-
-    // PI control algo begin
-    error     = Ref_v-adcresult;
-    PI_output = (kp*(error-prev_error)+ki*Ts*error)+prev_out;
-    PI_out = 330-PI_output;
-    // anti-windup
-    if(PI_output > out_max){
-        PI_output = out_max;
-    }
-    else if (PI_output < out_min) {
-        PI_output = out_min;
-    }
-
-    // defining the u(k-1) and e(k-1)
-    prev_out   = PI_output;
-    prev_error = error;
-    set_duty(PI_out); //
-  //  set_duty(PI_output);     // set PWM to the PI regulated value
-
-    ADC_clearIntFlag(myAdc, ADC_IntNumber_1);   // Clear ADCINT1 flag
-    PIE_clearInt(myPie, PIE_GroupNumber_10);    // Acknowledge interrupt
-    return;
-}
+void main(void)
 
 
-void main(void) {
+{
 
-   myAdc   = ADC_init((void *)ADC_BASE_ADDR, sizeof(ADC_Obj));
-   myClk   = CLK_init((void *)CLK_BASE_ADDR, sizeof(CLK_Obj));
-   myCpu   = CPU_init((void *)NULL, sizeof(CPU_Obj));
-   myFlash = FLASH_init((void *)FLASH_BASE_ADDR, sizeof(FLASH_Obj));
-   myGpio  = GPIO_init((void *)GPIO_BASE_ADDR, sizeof(GPIO_Obj));
-   myPie   = PIE_init((void *)PIE_BASE_ADDR, sizeof(PIE_Obj));
-   myPll   = PLL_init((void *)PLL_BASE_ADDR, sizeof(PLL_Obj));
-   myTimer = TIMER_init((void *)TIMER0_BASE_ADDR, sizeof(TIMER_Obj));
-   myWDog  = WDOG_init((void *)WDOG_BASE_ADDR, sizeof(WDOG_Obj));
-   myPwm4  = PWM_init((void *)PWM_ePWM4_BASE_ADDR, sizeof(PWM_Obj));
-   myPwr   = PWR_init((void *)PWR_BASE_ADDR, sizeof(PWR_Obj));
+    CPU_Handle myCpu;
+    PLL_Handle myPll;
+    WDOG_Handle myWDog;
 
-   disable();
-    // basic initialization
+    // Initialize all the handles needed for this application
+    myAdc = ADC_init((void *)ADC_BASE_ADDR, sizeof(ADC_Obj));
+    myClk = CLK_init((void *)CLK_BASE_ADDR, sizeof(CLK_Obj));
+    myCpu = CPU_init((void *)NULL, sizeof(CPU_Obj));
+    myFlash = FLASH_init((void *)FLASH_BASE_ADDR, sizeof(FLASH_Obj));
+    myGpio = GPIO_init((void *)GPIO_BASE_ADDR, sizeof(GPIO_Obj));
+    myPie = PIE_init((void *)PIE_BASE_ADDR, sizeof(PIE_Obj));
+    myPll = PLL_init((void *)PLL_BASE_ADDR, sizeof(PLL_Obj));
+    myPwm2 = PWM_init((void *)PWM_ePWM2_BASE_ADDR, sizeof(PWM_Obj));
+    myPwm1 = PWM_init((void *)PWM_ePWM1_BASE_ADDR, sizeof(PWM_Obj));
+    myWDog = WDOG_init((void *)WDOG_BASE_ADDR, sizeof(WDOG_Obj));
+
+    // Perform basic system initialization
     WDOG_disable(myWDog);
     CLK_enableAdcClock(myClk);
+    (*Device_cal)();
+
+    //Select the internal oscillator 1 as the clock source
     CLK_setOscSrc(myClk, CLK_OscSrc_Internal);
-    PLL_setup(myPll, PLL_Multiplier_10, PLL_DivideSelect_ClkIn_by_1);
 
-    enable();
-    ADC_INIT_Fn();
-    ADC_SETUP_Fn();
+    // Setup the PLL for x12 /2 which will yield 60Mhz = 10Mhz * 12 / 2
+    PLL_setup(myPll, PLL_Multiplier_12, PLL_DivideSelect_ClkIn_by_2);
 
-    //GPIO 6
-    GPIO_setMode(myGpio, GPIO_Number_6, GPIO_6_Mode_EPWM4A);
+    // Disable the PIE and all interrupts
+    PIE_disable(myPie);
+    PIE_disableAllInts(myPie);
+    CPU_disableGlobalInts(myCpu);
+    CPU_clearIntFlags(myCpu);
 
-      CLK_disableTbClockSync(myClk);
-      pwm_Init_();
+// If running from flash copy RAM only functions to RAM
+#ifdef _FLASH
+    memcpy(&RamfuncsRunStart, &RamfuncsLoadStart, (size_t)&RamfuncsLoadSize);
+#endif
 
-      CLK_enableTbClockSync(myClk);
+    // Setup a debug vector table and enable the PIE
+    PIE_setDebugIntVectorTable(myPie);
+    PIE_enable(myPie);
+    PIE_registerPieIntHandler(myPie, PIE_GroupNumber_10, PIE_SubGroupNumber_1,
+                                    (intVec_t)&adc_isr);
+    InitEPwm1();
 
-    while(1) {
-     ADC_forceConversion(myAdc, ADC_SocNumber_0);// Wait for ADC interrupt
+    // Initialize the ADC
+    ADC_enableBandGap(myAdc);
+    ADC_enableRefBuffers(myAdc);
+    ADC_powerUp(myAdc);
+    ADC_enable(myAdc);
+    ADC_setVoltRefSrc(myAdc, ADC_VoltageRefSrc_Int);
+
+    // Enable ADCINT1 in PIE
+         PIE_enableAdcInt(myPie, ADC_IntNumber_1);
+         // Enable CPU Interrupt 1
+         CPU_enableInt(myCpu, CPU_IntNumber_10);
+         // Enable Global interrupt INTM
+         CPU_enableGlobalInts(myCpu);
+         // Enable Global real time interrupt DBGM
+         CPU_enableDebugInt(myCpu);
+
+    LoopCount = 0;
+    ConversionCount = 0;
+
+        ADC_setIntPulseGenMode(myAdc, ADC_IntPulseGenMode_Prior);               //ADCINT1 trips after AdcResults latch
+        ADC_enableInt(myAdc, ADC_IntNumber_1);                                  //Enabled ADCINT1
+        ADC_setIntMode(myAdc, ADC_IntNumber_1, ADC_IntMode_ClearFlag);          //Disable ADCINT1 Continuous mode
+        ADC_setIntSrc(myAdc, ADC_IntNumber_1, ADC_IntSrc_EOC2);                 //setup EOC2 to trigger ADCINT1 to fire
+        ADC_setSocChanNumber (myAdc, ADC_SocNumber_0, ADC_SocChanNumber_A4);    //set SOC0 channel select to ADCINA4
+        ADC_setSocChanNumber (myAdc, ADC_SocNumber_1, ADC_SocChanNumber_A4);    //set SOC1 channel select to ADCINA4
+        ADC_setSocChanNumber (myAdc, ADC_SocNumber_2, ADC_SocChanNumber_A2);    //set SOC2 channel select to ADCINA2
+        ADC_setSocTrigSrc(myAdc, ADC_SocNumber_0, ADC_SocTrigSrc_EPWM2_ADCSOCA);    //set SOC0 start trigger on EPWM1A, due to round-robin SOC0 converts first then SOC1
+        ADC_setSocTrigSrc(myAdc, ADC_SocNumber_1, ADC_SocTrigSrc_EPWM2_ADCSOCA);    //set SOC1 start trigger on EPWM1A, due to round-robin SOC0 converts first then SOC1
+        ADC_setSocTrigSrc(myAdc, ADC_SocNumber_2, ADC_SocTrigSrc_EPWM2_ADCSOCA);    //set SOC2 start trigger on EPWM1A, due to round-robin SOC0 converts first then SOC1, then SOC2
+
+        ADC_setSocSampleWindow(myAdc, ADC_SocNumber_0, ADC_SocSampleWindow_12_cycles);   //set SOC0 S/H Window to 7 ADC Clock Cycles, (6 ACQPS plus 1)
+        ADC_setSocSampleWindow(myAdc, ADC_SocNumber_1, ADC_SocSampleWindow_12_cycles);   //set SOC1 S/H Window to 7 ADC Clock Cycles, (6 ACQPS plus 1)
+        ADC_setSocSampleWindow(myAdc, ADC_SocNumber_2, ADC_SocSampleWindow_12_cycles);   //set SOC2 S/H Window to 7 ADC Clock Cycles, (6 ACQPS plus 1)
+
+    // Enable PWM clock
+    CLK_enablePwmClock(myClk, PWM_Number_2);
+
+    // Setup PWM
+    PWM_enableSocAPulse(myPwm2);                                         // Enable SOC on A group
+    PWM_setSocAPulseSrc(myPwm2, PWM_SocPulseSrc_CounterEqualCmpAIncr);   // Select SOC from from CPMA on upcount
+    PWM_setSocAPeriod(myPwm2, PWM_SocPeriod_FirstEvent);                 // Generate pulse on 1st event
+    PWM_setCmpA(myPwm2, 0x0000);                                         // Set compare A value
+    PWM_setPeriod(myPwm2, 0x05DC);                                       // Period = 1388 for 5Khz Sampling Frequency
+    PWM_setCounterMode(myPwm2, PWM_CounterMode_Up);                      // count up and start
+    CLK_enableTbClockSync(myClk);
+
+    // Wait for ADC interrupt
+    for(;;)
+    {
+        PWM_setCmpA(myPwm1,Vpi_5);                  //TBPRD = 150
     }
 }
-void disable() {
-  // Disable function
-  PIE_disable(myPie);
-  PIE_disableAllInts(myPie);
-  CPU_disableGlobalInts(myCpu);
-  CPU_clearIntFlags(myCpu);
+
+void InitEPwm1()
+{
+    CLK_disableTbClockSync(myClk);
+    CLK_enablePwmClock(myClk, PWM_Number_1);
+    GPIO_setPullUp(myGpio, GPIO_Number_0, GPIO_PullUp_Disable);
+    GPIO_setPullUp(myGpio, GPIO_Number_1, GPIO_PullUp_Disable);
+    GPIO_setMode(myGpio, GPIO_Number_0, GPIO_0_Mode_EPWM1A);
+    GPIO_setMode(myGpio, GPIO_Number_1, GPIO_1_Mode_EPWM1B);
+     // Setup Sync
+    PWM_setSyncMode(myPwm1, PWM_SyncMode_EPWMxSYNC);
+  //  PWM_setSyncMode(myPwm2, PWM_SyncMode_EPWMxSYNC);
+    // Allow each timer to be sync'ed
+    PWM_enableCounterLoad(myPwm1);
+    PWM_setPeriod(myPwm1, PWM1_TIMER_TBPRD);
+    PWM_setCounterMode(myPwm1, PWM_CounterMode_Up);         // Count up
+    PWM_setIntMode(myPwm1, PWM_IntMode_CounterEqualZero);   // Select INT on Zero event
+    PWM_enableInt(myPwm1);                                  // Enable INT
+    PWM_setIntPeriod(myPwm1, PWM_IntPeriod_FirstEvent);     // Generate INT on 1st event
+
+    PWM_setActionQual_Period_PwmA(myPwm1, PWM_ActionQual_Set);
+    PWM_setActionQual_CntUp_CmpA_PwmA(myPwm1, PWM_ActionQual_Clear);
+
+
+    PWM_setActionQual_Period_PwmB(myPwm1, PWM_ActionQual_Clear);
+    PWM_setActionQual_CntUp_CmpA_PwmB(myPwm1, PWM_ActionQual_Set);
+
+    CLK_enableTbClockSync(myClk);
+
 }
 
-void enable() {
-   // Enable function
-  PIE_enable(myPie);
-  CPU_enableInt(myCpu, CPU_IntNumber_10);
-  CPU_enableGlobalInts(myCpu);
-  CPU_enableDebugInt(myCpu);
+__interrupt void adc_isr(void)
 
-  PIE_enableInt(myPie, PIE_GroupNumber_3, PIE_InterruptSource_XINT_1);
-  CPU_enableInt(myCpu, CPU_IntNumber_1);
-  GPIO_setExtInt(myGpio, GPIO_Number_12, CPU_ExtIntNumber_1);
-  PIE_setExtIntPolarity(myPie, CPU_ExtIntNumber_1, PIE_ExtIntPolarity_RisingEdge);
-  PIE_enableExtInt(myPie, CPU_ExtIntNumber_1);
+ {
+     Voltage1[ConversionCount] = ADC_readResult(myAdc, ADC_ResultNumber_1);
+     sum += Voltage1[ConversionCount];
+      if(ConversionCount == 19)
+      {
+        Vout_count = sum/ConversionCount;  // average count
+        Va = (Vout_count * 3.3)/(4096) ;  // sensed voltage in 3.3V format
+        err_5 = 0.7 - Va;
+
+        A_5 = (err_5 * Kp); // kp multiplication
+        B_5 = (err_5 * Ki); // Ki multiplication
+        v_5 = k_5 + (B_5 + in_A);
+        if (v_5 >= 300) v_5 = 300;  // maximum duty cycle limit
+        if (v_5 <= 00) v_5 = 00;
+        k_5 = v_5;
+        in_A = B_5;
+        Vpi_5 = A_5 + v_5;
+        if (Vpi_5 >= 300) Vpi_5 = 280;
+        if (Vpi_5 <= 01)  Vpi_5 = 20;                 // minimum duty cycle limit
+        if (Va > 3.0) Vpi_5 = 20;
+    //    if (err_5< 0) Vpi_5= 0;
+        ConversionCount = 0;
+        sum=0;
+      }
+      else ConversionCount++;
+    // Clear ADCINT1 flag reinitialize for next SOC
+    ADC_clearIntFlag(myAdc, ADC_IntNumber_1);
+    // Acknowledge interrupt to PIE
+    PIE_clearInt(myPie, PIE_GroupNumber_10);
+  //  return;
 }
-
-void ADC_INIT_Fn() {
-  ADC_enableBandGap(myAdc);
-  ADC_enableRefBuffers(myAdc);
-  ADC_powerUp(myAdc);
-  ADC_enable(myAdc);
-  ADC_setVoltRefSrc(myAdc, ADC_VoltageRefSrc_Int);
-}
-
-void ADC_SETUP_Fn() {
-  PIE_registerPieIntHandler(myPie, PIE_GroupNumber_10, PIE_SubGroupNumber_1, (intVec_t)&adc_isr);
-  PIE_enableAdcInt(myPie, ADC_IntNumber_1);  // Enable ADCINT1 in PIE
-
-  ADC_setIntPulseGenMode(myAdc, ADC_IntPulseGenMode_Prior);
-  ADC_enableInt(myAdc, ADC_IntNumber_1);                                  //Enabled ADCINT1
-  ADC_setIntMode(myAdc, ADC_IntNumber_1, ADC_IntMode_ClearFlag);
-  ADC_setIntSrc(myAdc, ADC_IntNumber_1, ADC_IntSrc_EOC0);
-  ADC_setSocChanNumber (myAdc, ADC_SocNumber_0, ADC_SocChanNumber_A4);
-  ADC_setSocTrigSrc(myAdc, ADC_SocNumber_0, ADC_SocTrigSrc_Sw);
-  ADC_setSocSampleWindow(myAdc, ADC_SocNumber_0, ADC_SocSampleWindow_7_cycles);
-}
-
-void pwm_Init_() {
-    CLK_enablePwmClock(myClk, PWM_Number_4);
-    // Setup TBCLK
-    PWM_setPeriod(myPwm4, TBPRD);   // Set timer period
-    PWM_setPhase(myPwm4, 0x0000);   // set the phase
-    PWM_setCount(myPwm4, 0x0000);   // set the count
-
-    // Setup counter mode
-    PWM_setCounterMode(myPwm4, PWM_CounterMode_UpDown);
-    PWM_disableCounterLoad(myPwm4);
-
-    // clk division
-    PWM_setHighSpeedClkDiv(myPwm4, PWM_HspClkDiv_by_1);
-    PWM_setClkDiv(myPwm4, PWM_ClkDiv_by_1);
-
-    PWM_setShadowMode_CmpA(myPwm4, PWM_ShadowMode_Shadow);
-    PWM_setLoadMode_CmpA(myPwm4, PWM_LoadMode_Zero);
-    PWM_setActionQual_CntUp_CmpA_PwmA(myPwm4, PWM_ActionQual_Clear);
-    PWM_setActionQual_CntDown_CmpA_PwmA(myPwm4, PWM_ActionQual_Set);  // Clear PWM1A on event A, down count
-}
-void set_duty( int a) {
- CMPA = a;
- PWM_setCmpA(myPwm4, CMPA);
- PWM_setCmpB(myPwm4, CMPA);// Set compare A value
-}
-
-//===========================================================================
-// No more. stop pls
